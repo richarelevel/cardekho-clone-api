de77757b20f16a3d48cab97e34ef30143c6bf20d
richasinhaa <richa.sinha0603@gmail.com>
2022-04-06 15:30:12 +0800

    Cardekho Clone API Template added.

---
+++ src/main/java/com/api/cardekhoclone/CardekhoCloneApplication.java
@@ -0,0 +1,13 @@
+package com.api.cardekhoclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardekhoCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardekhoCloneApplication.class, args);
+	}
+
+}
